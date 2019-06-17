797085221be19ed4bc26d14d8676c516986d5bf6
Timothee DELABROUILLE <tdelabro@e3r7p11.42.fr>
2019-06-17 21:45:19 +0200

    gestion du repertoire /dev/fd

--- src/list_and_rec.c
+++ src/list_and_rec.c
@@ -6,7 +6,7 @@
 /*   By: tdelabro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/25 17:26:31 by tdelabro          #+#    #+#             */
-/*   Updated: 2019/06/17 19:58:35 by tdelabro         ###   ########.fr       */
+/*   Updated: 2019/06/17 21:31:14 by tdelabro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 static void	ft_disp_and_rec(t_list *dir_to_rec, t_list *dir_to_disp,\
 				const unsigned int options)
 {
-	ft_get_args_stat(&dir_to_disp);
-	ft_get_args_stat(&dir_to_rec);
 	ft_order_dirs(&dir_to_disp, options);
 	ft_order_dirs(&dir_to_rec, options);
 	ft_display(dir_to_disp, options, TRUE);
@@ -29,20 +27,26 @@ static void	ft_readdir(DIR *dirp, t_dir *current, const unsigned int options)
 	struct dirent	*dir;
 	t_list			*dir_to_rec;
 	t_list			*dir_to_disp;
+	t_list			*head;
 
 	dir_to_rec = NULL;
 	dir_to_disp = NULL;
 	while ((dir = readdir(dirp)) != NULL)
-	{
 		if ((options & OPT_ALL) || dir->d_name[0] != '.')
 			ft_make_tdir(&dir_to_disp, current->full, dir->d_name, options);
-		if ((options & OPT_REC) && dir->d_type == DT_DIR\
-			&& ((ft_strequ(dir->d_name, ".") == 0\
-			&& ft_strequ(dir->d_name, "..") == 0)\
-			&& ((options & OPT_ALL) || dir->d_name[0] != '.')))
-			ft_make_tdir(&dir_to_rec, current->full, dir->d_name, options);
-	}
 	closedir(dirp);
+	ft_get_args_stat(&dir_to_disp);
+	head = dir_to_disp;
+	while (head)
+	{
+		if ((options & OPT_REC)\
+			&& S_ISDIR(((t_dir*)head->content)->fstat->st_mode)
+			&& ft_strequ(((t_dir*)head->content)->name, ".") == 0\
+			&& ft_strequ(((t_dir*)head->content)->name, "..") == 0)
+			ft_make_tdir(&dir_to_rec, current->full,\
+			((t_dir*)head->content)->name, options);
+		head = head->next;
+	}
 	ft_disp_and_rec(dir_to_rec, dir_to_disp, options);
 	ft_del_tdir(current);
 }
